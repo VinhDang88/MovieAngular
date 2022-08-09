410498c12ef44bfca91d61bcd12a53ee7374cd65
VinhDang88 <vincedang720@gmail.com>
2022-08-09 18:25:04 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+{path: 'movie-list', component: MovieListComponent},
+{path: 'movie-details/:id', component: MovieDetailsComponent}
+
+
+  
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieListComponent,
+    MovieDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
